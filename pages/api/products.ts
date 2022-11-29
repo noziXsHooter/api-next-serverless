a86f9e12b4de676e52a03ab325e9c7e3c1663cbb
noziX <94942615+noziXsHooter@users.noreply.github.com>
2022-11-29 02:32:15 -0300

    setHeaders in next.config.js file

--- pages/api/products.ts
+++ pages/api/products.ts
@@ -28,6 +28,7 @@ let cachedDb: Db;
             
                case 'POST': {
                   return postProduct(req, res); 
+
               }
           }                 
     }
@@ -71,27 +72,7 @@ let cachedDb: Db;
         return res.status(500).json({message: e})
       }
     }
-/* 
-    async function deleteProduct(req: VercelRequest ,res: VercelResponse) 
-    {
-      try {
-        //connect to database
-        const uri = 'mongodb+srv://ecommercetest:ecommercetest@ecommercetest.hlmhtv7.mongodb.net/?retryWrites=true&w=majority';
-        const db = await connectToDatabase(uri);
-        const collection = db.collection('products');
-        const list = await collection.find().toArray();
 
-        return res.status(201).json(list)
-                  .setHeader('Access-Control-Allow-Origin:','*');
-                 .setHeader('Content-Type:','application/json')
-                .setHeader('Set-Cookie:', '<JSESSIONID>=<x6Bs790Fd4Fsdg43fdGhx>; Secure'); 
-
-              } catch(e) {
-                res.status(500).json({message: e})
-              }
-            }
-*/
-    
    /*  if (req.method === 'POST'){
 
       const { name , image, price } = req.body
